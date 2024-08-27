9e5f55b9499c24b03f97ff405dfc6d57ea9d4df5
Truong Nguyen <truong.nguyen201@gmail.com>
2024-08-26 23:06:14 -0400

    Intial nasa app

---
+++ src/Components/SideBar.jsx
@@ -0,0 +1,11 @@
+export default function SideBar() {
+    return (
+        <div>
+        <h2>The Brutal Matian Landscape</h2>
+        <di>
+            <p>Description</p>
+            <p>alsdkjas kajdlaskd</p>
+        </di>
+        </div>
+    )
+}
