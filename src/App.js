fd224ae8323397c4308f30db0eeff2f041d2093b
Truong Nguyen <truong.nguyen201@gmail.com>
2024-08-27 01:57:03 -0400

    Add fetching API logic

--- src/App.js
+++ src/App.js
@@ -1,14 +1,46 @@
+import { useState, useEffect } from 'react';
 import Main from "./Components/Main";
 import SideBar from "./Components/SideBar";
 import Footer from "./Components/Footer";
 
 function App() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+
+  function handleToggleModal() {
+    setShowModal(!showModal)
+  }
+
+  // fetching data from NASA API
+  useEffect(() => {
+    async function fetchAPIData() {
+      const NASA_KEY = process.env.REACT_APP_NASA_API_KEY;
+      const url = `https://api.nasa.gov/planetary/apod?api_key=${NASA_KEY}`;
+      try {
+        const res = await fetch(url);
+        const apiData = await res.json();
+        setData(apiData)
+        console.log('DATA\n', apiData)
+      } catch (err) {
+        console.log(err.message);
+      }
+    }
+    // fetchAPIData();
+  }, []);
+
   // What th App function returns
   return (
     <>
-    <Main />
-    <SideBar></SideBar>
-    <Footer />
+    {data ? (<Main />): (
+      <div className="loadingState">
+        <i className="fa-solid fa-gear"></i>
+      </div>
+    )}
+    {/* {Conditaionlly renders SideBar if showModal is true} */}
+    {showModal && (<SideBar handleToggleModal={handleToggleModal} />)}
+    {/* {Passing props showModal to the footer, allowing the Footer component to receive the props} */}
+    {data && (<Footer handleToggleModal={handleToggleModal}/>)}
     </>
   );
 }
--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -1,14 +1,15 @@
-export default function SideBar() {
+export default function SideBar(props) {
+    const {handleToggleModal} = props
     return (
         <div className="sidebar">
-            <div className="bgOverlay"></div>
+            <div className="bgOverlay" onClick={handleToggleModal}></div>
             <div className="sidebarContents">
                 <h2>The Brutal Matian Landscape</h2>
                 <div>
                     <p>Description</p>
                     <p>alsdkjas kajdlaskd</p>
                 </div>
-                <button>
+                <button onClick={handleToggleModal}>
                     <i className="fa-solid fa-arrow-right"></i>
                 </button>
             </div>
